f37da26dea8de18cff259ee447e4c8b8d4624709
VilmaLopez <127805510+VilmaLopez@users.noreply.github.com>
2023-06-19 21:49:59 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,98 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'AppBase',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/contenido/',
+    	url: 'contenido.html',
+    	name: 'contenido',
+  		},
+		{
+		path: '/zapatos/',
+    	url: 'zapatos.html',
+    	name: 'zapatos',
+		},
+		{
+		path: '/zapato_producto/',
+    	url: 'zapato_producto.html',
+    	name: 'zapato_producto',
+		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+		},
+		
+	],
+	dialog: {
+		title: 'AppBase',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('!Welcome to AppBase !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnAddCart', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
